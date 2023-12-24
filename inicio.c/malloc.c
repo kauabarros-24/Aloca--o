2c1e281241df034f2e6dd71ee46312f27787d26d
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-24 15:56:50 -0300

    malloc

--- inicio.c/malloc.c
+++ inicio.c/malloc.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int main () {
-    char *p;
-    p = (char*)malloc(sizeof(char)*2);
-    p[0] = 'a';
-    *(p+1) = 'b';
-    printf("%d %d\n", p[0], p[1]);
-    free(p);
+    int *p1, *p2;
+    p1 = (int*)malloc(2*sizeof(int));
+    p2 = (int*)calloc(2, sizeof(int));
+    printf("%d\n%d\n\n", p1[0], p1[1]);
+    printf("%d\n%d\n\n", p2[0], p2[1]);
+
 }
