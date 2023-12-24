a901e54ff1af4c46f627ca73ccc4330a924a2398
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-24 16:06:27 -0300

    realloc

--- inicio.c/realloc.c
+++ inicio.c/realloc.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int main() {        
-    int *p, i;
+    int *p;
+    int i; 
     p = (int*)malloc(sizeof(int)*2);
-    p = (int*)realloc(p, sizeof(int)*20);  
+    p = (int*)realloc(p, sizeof(int)*20);
     for (i = 0; i < 20; i++) {
         p[i] = i;
+        printf("%d\n", p);
     }
-    p = (int*)realloc(p, sizeof(int)*2);
-    printf("Local final = %u\n", p);
 }
