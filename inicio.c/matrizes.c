74188eb817c3251cbc47a46f074642b702b2c794
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-24 16:40:37 -0300

    f

--- inicio.c/matrizes.c
+++ inicio.c/matrizes.c
@@ -2,5 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-    
+    int **p, i, j, k, n = 4, m = 3;
+    p = (int**)malloc(sizeof(int*)*n);
+    for (i = 0; i < n; i++) {
+        p[i] = (int*)malloc(sizeof(int)*m);
+    }
 }
