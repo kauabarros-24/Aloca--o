ca3a1e142eca0dfe1922bf3f95041c7181a29a70
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-24 16:27:47 -0300

    adadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadada

--- inicio.c/realloc.c
+++ inicio.c/realloc.c
@@ -5,9 +5,12 @@ int main() {
     int *p;
     int i; 
     p = (int*)malloc(sizeof(int)*2);
+    printf("Local inicial: %u\n", p);
     p = (int*)realloc(p, sizeof(int)*20);
+    printf("Local do meio: %u\n", p);
     for (i = 0; i < 20; i++) {
         p[i] = i;
-        printf("%d\n", p);
     }
+    p = (int*)realloc(p, sizeof(int)*20);
+    printf("Local Final: %u\n", p);
 }
